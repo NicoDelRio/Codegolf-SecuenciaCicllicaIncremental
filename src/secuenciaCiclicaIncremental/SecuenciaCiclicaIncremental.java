5ae1aba07b7ab3d1874e12f9c7fc05fb27d0df68
NicoDelRio <nicolas-delrio@hotmail.com>
2018-09-17 12:29:25 -0300

    Con 98 caracteres

--- src/secuenciaCiclicaIncremental/SecuenciaCiclicaIncremental.java
+++ src/secuenciaCiclicaIncremental/SecuenciaCiclicaIncremental.java
@@ -2,15 +2,23 @@ package secuenciaCiclicaIncremental;
 
 public class SecuenciaCiclicaIncremental {
 
-	// 118 Caracteres sin espacios(desde el "boolean..."  hasta la "}"
+//	// 118 Caracteres sin espacios(desde el "boolean..."  hasta la "}"
+//	public boolean secuenciaCiclica(int s[]) {
+//		int c = 0, i = 0, l = s.length-1;
+//		while (i < l)
+//			if(s[i++] > s[i])
+//				c++;
+//		if(l > 1 && s[l] > s[0])
+//			c++;
+//		return c < 2;
+//	}
+	
+	// 98 Caracteres sin espacios(desde el "boolean..."  hasta la "}"
 	public boolean secuenciaCiclica(int s[]) {
-		int c = 0, i = 0, l = s.length-1;
+		int c = 0, i = 0, l = s.length;
 		while (i < l)
-			if(s[i++] > s[i])
+			if(s[i++%l] > s[i%l])
 				c++;
-		if(l > 1 && s[l] > s[0])
-			c++;
 		return c < 2;
 	}
-	
 }
